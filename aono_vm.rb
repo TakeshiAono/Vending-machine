3ea51e3928f53ef58c3bbf3a4108fe296c3a8d80
aono <takeshi.aono001@gmail.com>
2022-07-11 11:34:52 +0900

    init

---
+++ aono_vm.rb
@@ -0,0 +1,60 @@
+class VendingMachine
+
+MONEY = [10, 50, 100, 500, 1000]
+
+  def initialize
+    @input_money = 0
+    @drinks = {cola: {price: 120, volume: 5, name: "コーラ"}, water: {price: 100, volume: 5, name: "お水" }, redbull: {price: 200, volume: 5, name: "レッドブル" }}
+    @seles = 0
+  end
+
+  def input_setting(money)#もし投入額が定数じゃなかったら投入額をそのまま返す投入額が定数だったら11行目を実行
+    if MONEY.include?(money)
+    @input_money += money
+    else
+      puts "#{money}円は使えねーよ！"
+    end
+  end
+
+  def input_back
+    puts @input_money
+    @input_money = 0
+  end
+
+  def stocks_info
+    @drinks.each do |drink, info|
+        info.each do |key, value|
+        puts "#{drink}の#{key}は#{value}です"
+      end
+    end
+  end
+
+  def can_buy_cola
+    if @input_money >= @drinks[:cola][:price] && @drinks[:cola][:volume] > 0
+      puts "You get cola!"
+      puts @drinks[:cola][:volume] -= 1
+      @input_money -= @drinks[:cola][:price]
+      seles_sum
+      input_back
+    else
+      puts "You don't get!"
+    end
+  end
+
+  def seles_sum
+    @seles += @drinks[:cola][:price]
+  end
+
+  def can_buy_list
+    @drinks.each do |drink, info|
+      info.each do |key, value|
+        if key == :price && value <= @input_money
+          if @drinks[drink][:volume] != 0
+            puts drink
+          end
+        end
+      end
+    end
+  end
+
+end
